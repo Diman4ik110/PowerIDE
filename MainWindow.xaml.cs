9eb4fcdead5152b42f3c763f74a63105c5c4b119
Diman4ik110 <dvu.dima@mail.ru>
2020-03-27 19:50:08 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Editing;
+using ICSharpCode.AvalonEdit.Document;
+using System.Xml;
+using Microsoft.Win32;
+
+
+namespace PowerShell_Studio_Free
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    ///
+    public partial class MainWindow : Window
+    {
+        public int Radius = 10;
+        public class MyCompletionData : ICompletionData
+        {
+
+            public MyCompletionData(string text)
+            {
+                this.Text = text;
+            }
+
+            public System.Windows.Media.ImageSource Image
+            {
+                get { return null; }
+            }
+
+            public string Text { get; private set; }
+
+            // Use this property if you want to show a fancy UIElement in the list.
+            public object Content
+            {
+                get { return this.Text; }
+            }
+
+            public object Description
+            {
+                get { return "Description for " + this.Text; }
+            }
+            double ICompletionData.Priority {
+                get;
+            }
+
+            public void Complete(TextArea textArea, ISegment completionSegment,
+                EventArgs insertionRequestEventArgs)
+            {
+                textArea.Document.Replace(completionSegment, this.Text);
+            }
+
+        }
+        private OpenFileDialog fileDialog = new OpenFileDialog();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            fileDialog.Filter = "PowerShell scripts (*.ps1)|*.ps1";
+            TextEdit.TextArea.TextEntering += TextArea_TextEntering;
+            TextEdit.TextArea.TextEntered += TextArea_TextEntered;
+            //item.Style = ;
+        }
+        CompletionWindow completionWindow;
+        private void TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == ".")
+            {
+                // Open code completion after the user has pressed dot:
+                completionWindow = new CompletionWindow(TextEdit.TextArea);
+                IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+                data.Add(new MyCompletionData("Item1"));
+                data.Add(new MyCompletionData("Item2"));
+                data.Add(new MyCompletionData("Item3"));
+                completionWindow.Show();
+                completionWindow.Closed += delegate {
+                    completionWindow = null;
+                };
+            }
+        }
+
+        private void TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Maximize(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            
+        }
+
+        private void Button_Minimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath;
+            if (fileDialog.ShowDialog() == true)
+            {
+                filePath = fileDialog.FileName;
+                TextEdit.Load(filePath);
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NewProject_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
